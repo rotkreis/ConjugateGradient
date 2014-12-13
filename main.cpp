ea0005df638af22976a908d0066f7361c9f3699e
rotkreis <rotkreis@gmail.com>
2014-12-13 11:29:32 +0800

    Finished

--- main.cpp
+++ main.cpp
@@ -7,8 +7,65 @@
 //
 
 #include <iostream>
+#include <cmath>
 #include "Matrix.h"
 #include "CG.h"
+#include "IterativeMethods.h"
+
+double f(double x, double y){
+    return sin(x * y);
+}
+double boundary(double x, double y){
+    return x*x + y*y;
+}
+
+void problem1(int N){
+    int nNodes = N - 1;
+    double h = 1.0 / N;
+    Matrix A(nNodes * nNodes, nNodes * nNodes);
+    mVector rhs(nNodes * nNodes);
+    for (int i = 0; i != nNodes; i++) {
+        for (int j = 0; j != nNodes; j++) {
+            rhs[i * nNodes + j] = h * h / 4.0 * f((i + 1) * h, (j + 1) * h);
+            if (i == 0) {
+                rhs[i * nNodes + j] += boundary(0, (j + 1) * h) / 4;
+            }
+            if (i == nNodes - 1) {
+                rhs[i * nNodes + j] += boundary(1, (j + 1) * h) / 4;
+            }
+            if (j == 0) {
+                rhs[i * nNodes + 1] += boundary((i + 1) * h, 0) / 4;
+            }
+            if (j == nNodes - 1) {
+                rhs[i * nNodes + 1] += boundary((i + 1) * h, 1) / 4;
+            }
+        }
+    }
+    for (int i = 0; i != nNodes; i++) {
+        for (int j = 0; j != nNodes; j++) {
+            A(i * nNodes + j, i * nNodes + j) = 1 + h * h / 4;
+            if (i != 0) {
+                A(i * nNodes + j, (i - 1) * nNodes + j) = -1.0 / 4;
+            }
+            if (i != nNodes - 1) {
+                A(i * nNodes + j, (i + 1) * nNodes + j) = -1.0 / 4;
+            }
+            if (j != 0) {
+                A(i * nNodes + j, i * nNodes + j - 1) = -1.0 / 4;
+            }
+            if (j != nNodes - 1) {
+                A(i * nNodes + j, i * nNodes + j + 1) = -1.0 / 4;
+            }
+        }
+    }
+    mVector guess(nNodes * nNodes);
+    for (int i = 0; i != guess.dim(); i++) {
+        guess[i] = 5;
+    }
+    mVector res(nNodes * nNodes);
+    std::cout << CGSolver(A, rhs, res, guess, 10E-10, 100) << std::endl;
+    std::cout << res;
+}
 
 static Matrix CreateHilbertMatrix(int dim) {
     Matrix temp(dim,dim);
@@ -19,7 +76,6 @@ static Matrix CreateHilbertMatrix(int dim) {
     }
     return temp;
 }
-
 static mVector CreateHilbertRHS(int dim) {
     mVector temp(dim);
     for (int i = 0; i != dim; i++) {
@@ -31,29 +87,38 @@ static mVector CreateHilbertRHS(int dim) {
 }
 
 int main(int argc, const char * argv[]) {
-//    mVector v(2);
-//    v[0] = 1;
-//    v[1] = 2;
-//    Matrix A(2,2);
-//    A = {1,2,3,4};
-//    mVector p = A * v;
-//    
-//    mVector x(4);
-//    x = {.33,.33,.33,.33};
-//    Matrix h = CreateHilbertMatrix(4);
-//    mVector r = CreateHilbertRHS(4);
-//    std::cout << h << r << std::endl;
-//    std::cout << r - h * x;
-    int dim = 10;
-    mVector res(dim);
-    mVector guess(dim);
-    for (int i = 0; i != dim; i++) {
-        guess[i] = 1;
-    }
-    Matrix A = CreateHilbertMatrix(dim);
-    mVector b = CreateHilbertRHS(dim);
-    int kMax = 10000;
-    std::cout << CGSolver(A, b, res, guess, 10E-14, kMax) << endl;
-    std::cout << res;
+    //-------------- Problem 1
+//    problem1(20);
+  
+    
+    // -------------- Problem 2
+//    int dim = 20;
+//    mVector res(dim);
+//    mVector guess(dim);
+//    for (int i = 0; i != dim; i++) {
+//        guess[i] = 1;
+//    }
+//    Matrix A = CreateHilbertMatrix(dim);
+//    mVector b = CreateHilbertRHS(dim);
+//    std::cout << CGSolver(A, b, res, guess, 10E-14, 100) << endl;
+//    std::cout << res;
+    
+
+    // --------------Problem 3
+    Matrix prob3(5,5);
+    prob3 = {10,1,2,3,4,1,9,-1,2,-3,2,-1,7,3,-5,3,2,3,12,-1,4,-3,-5,-1,15};
+    mVector rhs3(5);
+    rhs3 = {12,-27,14,-17,12};
+    mVector res3(5);
+    mVector guess3(5);
+    mVector Jacobi(5);
+    mVector GS(5);
+    double precision = 10E-5;
+    std::cout <<"CG: " <<CGSolver(prob3, rhs3, res3, guess3, precision, 100) << std::endl;
+    std::cout << res3;
+    std::cout <<"Jacobi: "<< IterativeMethods::JacobiSolve(prob3, rhs3, guess3, Jacobi, precision) << std::endl;
+    std::cout << Jacobi;
+    std::cout << "GS: " << IterativeMethods::GaussSolve(prob3, rhs3, guess3, GS, precision) << std::endl;
+    std::cout << GS;
     return 0;
 }
